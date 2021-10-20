5972b62f9f84cd5f68531770572f2179184ea4c2
liao2000 <liao2000@livemail.tw>
2021-10-20 22:22:30 +0800

    Formatter

--- 資料結構上機考參考答案/第3次作業/題目2.c
+++ 資料結構上機考參考答案/第3次作業/題目2.c
@@ -1,94 +1,94 @@
-#include<stdio.h>
-#include<stdlib.h>
-void swap(int* a , int* b){
+#include <stdio.h>
+#include <stdlib.h>
+void swap(int* a, int* b) {
     int tmp;
-    tmp=*a;
-    *a=*b;
-    *b=tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-void inorder(int* heap, int index, int max_index, char* answer){
-    if(index<=max_index){
-        inorder(heap, index*2, max_index, answer);
-        printf("%d ",heap[index]);
-        sprintf(answer,"%s%d ",answer,heap[index]);
-        inorder(heap, index*2+1, max_index, answer);
+void inorder(int* heap, int index, int max_index, char* answer) {
+    if (index <= max_index) {
+        inorder(heap, index * 2, max_index, answer);
+        printf("%d ", heap[index]);
+        sprintf(answer, "%s%d ", answer, heap[index]);
+        inorder(heap, index * 2 + 1, max_index, answer);
     }
 }
 
-void adjust(int *heap, int i, int num_nodes){
-    //Compare its left and right
-    //Change the smaller one
-    //If this node has two children, left and right
-    if(i*2+1 <= num_nodes){
-        if(heap[i*2] > heap[i*2+1]){
-            if(heap[i] > heap[i*2+1]){
-                swap(&heap[i], &heap[i*2+1]);
-                adjust(heap, i*2+1, num_nodes);
+void adjust(int* heap, int i, int num_nodes) {
+    // Compare its left and right
+    // Change the smaller one
+    // If this node has two children, left and right
+    if (i * 2 + 1 <= num_nodes) {
+        if (heap[i * 2] > heap[i * 2 + 1]) {
+            if (heap[i] > heap[i * 2 + 1]) {
+                swap(&heap[i], &heap[i * 2 + 1]);
+                adjust(heap, i * 2 + 1, num_nodes);
             }
-        }else{
-            if(heap[i] > heap[i*2]){
-                swap(&heap[i], &heap[i*2]);
-                adjust(heap, i*2, num_nodes);
+        } else {
+            if (heap[i] > heap[i * 2]) {
+                swap(&heap[i], &heap[i * 2]);
+                adjust(heap, i * 2, num_nodes);
             }
         }
-    //If this node only has one child, left
-    }else if(i*2 <= num_nodes){
-        if(heap[i] > heap[i*2]){
-            swap(&heap[i], &heap[i*2]);
-            adjust(heap, i*2, num_nodes);
+        // If this node only has one child, left
+    } else if (i * 2 <= num_nodes) {
+        if (heap[i] > heap[i * 2]) {
+            swap(&heap[i], &heap[i * 2]);
+            adjust(heap, i * 2, num_nodes);
         }
     }
 }
-int main(){
-//Phase1: Read file and create heap
-    FILE* f=fopen("input2.txt","r");
-    if(!f){
+int main() {
+    // Phase1: Read file and create heap
+    FILE* f = fopen("input2.txt", "r");
+    if (!f) {
         fprintf(stderr, "Cannot open input2.txt");
         exit(1);
     }
-    int num_nodes=0;
+    int num_nodes = 0;
     char c;
-    for(c=getc(f); c!='\n'; c=getc(f)){
-        if(c==' '){
+    for (c = getc(f); c != '\n'; c = getc(f)) {
+        if (c == ' ') {
             num_nodes++;
         }
     }
     num_nodes++;
-    int* heap=malloc((num_nodes+1)*sizeof(int));
+    int* heap = malloc((num_nodes + 1) * sizeof(int));
     fclose(f);
 
-    f=fopen("input2.txt","r");
-    int i,j;
-    for(i=0; i<num_nodes; i++){
-        fscanf(f,"%d",&heap[i+1]);
+    f = fopen("input2.txt", "r");
+    int i, j;
+    for (i = 0; i < num_nodes; i++) {
+        fscanf(f, "%d", &heap[i + 1]);
     }
     fclose(f);
 
     printf("Before adjusting:\n");
-    for(i=0; i<num_nodes; i++){
-        printf("%d ",heap[i+1]);
+    for (i = 0; i < num_nodes; i++) {
+        printf("%d ", heap[i + 1]);
     }
     printf("\n");
 
-//Phase2: Adjust heap
-    for(i=num_nodes; i>0; i--){
+    // Phase2: Adjust heap
+    for (i = num_nodes; i > 0; i--) {
         adjust(heap, i, num_nodes);
     }
 
     printf("After adjusting:\n(Level order)\n");
-    for(i=0; i<num_nodes; i++){
-        printf("%d ",heap[i+1]);
+    for (i = 0; i < num_nodes; i++) {
+        printf("%d ", heap[i + 1]);
     }
     printf("\n");
 
-    char answer[64]="";
+    char answer[64] = "";
     printf("(In order)\n");
     inorder(heap, 1, num_nodes, answer);
     printf("\n");
 
-//Phase3: Write to file
-    f=fopen("output2.txt","w");
+    // Phase3: Write to file
+    f = fopen("output2.txt", "w");
     fprintf(f, "%s", answer);
     fclose(f);
 
--- 資料結構上機考參考答案/第4次作業/題目3.c
+++ 資料結構上機考參考答案/第4次作業/題目3.c
@@ -1,169 +1,172 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define INF 9999
 
-struct q{
+struct q {
     int data;
     struct q* next;
 };
 typedef struct q* queue;
 
-void enq(queue* q, int data){
-    queue cur,pre,new;
-    new=malloc(sizeof(*new));
-    new->data=data;
-    new->next=NULL;
-    if(*q){
-        for(cur=*q; cur; cur=cur->next){
-            pre=cur;
+void enq(queue* q, int data) {
+    queue cur, pre, new;
+    new = malloc(sizeof(*new));
+    new->data = data;
+    new->next = NULL;
+    if (*q) {
+        for (cur = *q; cur; cur = cur->next) {
+            pre = cur;
         }
-        pre->next=new;
-    }else{
-        *q=new;
+        pre->next = new;
+    } else {
+        *q = new;
     }
 }
 
-int deq(queue* q){
+int deq(queue* q) {
     int ret;
     queue tmp;
-    tmp=*q;
-    ret=(*q)->data;
-    *q=(*q)->next;
+    tmp = *q;
+    ret = (*q)->data;
+    *q = (*q)->next;
     free(tmp);
     return ret;
 }
 
-struct s{
-    int i,j;
+struct s {
+    int i, j;
     struct s* next;
 };
 typedef struct s* stack;
 
-void push(stack* s, int i, int j){
+void push(stack* s, int i, int j) {
     stack new;
-    new=malloc(sizeof(*new));
-    new->i=i;
-    new->j=j;
-    new->next=*s;
-    *s=new;
+    new = malloc(sizeof(*new));
+    new->i = i;
+    new->j = j;
+    new->next = *s;
+    *s = new;
 }
 
-stack pop(stack* s){
+stack pop(stack* s) {
     stack tmp;
-    tmp=*s;
-    *s=(*s)->next;
+    tmp = *s;
+    *s = (*s)->next;
     free(tmp);
     return *s;
 }
 
-int main(){
-//Phase1: Read file
-    FILE* f=fopen("input3.txt","r");
-    if(!f){
+int main() {
+    // Phase1: Read file
+    FILE* f = fopen("input3.txt", "r");
+    if (!f) {
         fprintf(stderr, "Cannot open input3.txt\n");
         exit(1);
     }
     char c;
-    int num_node=1;
-    for(c=getc(f); c!='\n'; c=getc(f)){
-        if(c==' '){
+    int num_node = 1;
+    for (c = getc(f); c != '\n'; c = getc(f)) {
+        if (c == ' ') {
             num_node++;
         }
     }
     fclose(f);
 
-    int i,j,tmp;
-    int** adj_matrix=calloc(num_node, sizeof(int*));
-    for(i=0; i<num_node; i++){
-        adj_matrix[i]=calloc(num_node, sizeof(int));
+    int i, j, tmp;
+    int** adj_matrix = calloc(num_node, sizeof(int*));
+    for (i = 0; i < num_node; i++) {
+        adj_matrix[i] = calloc(num_node, sizeof(int));
     }
-    f=fopen("input3.txt","r");
-    for(i=0; i<num_node; i++){
-        for(j=0; j<num_node; j++){
-            fscanf(f,"%d",&tmp);
-            if(tmp!=0){
-                adj_matrix[i][j]=tmp;
+    f = fopen("input3.txt", "r");
+    for (i = 0; i < num_node; i++) {
+        for (j = 0; j < num_node; j++) {
+            fscanf(f, "%d", &tmp);
+            if (tmp != 0) {
+                adj_matrix[i][j] = tmp;
             }
         }
     }
     fclose(f);
 
-    for(i=0; i<num_node; i++){
-        for(j=0; j<num_node; j++){
-            printf("%3d ",adj_matrix[i][j]);
+    for (i = 0; i < num_node; i++) {
+        for (j = 0; j < num_node; j++) {
+            printf("%3d ", adj_matrix[i][j]);
         }
         printf("\n");
     }
 
-//Phase2: Ford-Fulkerson (simplified)
-    //Reference: https://www.youtube.com/watch?v=GiN3jRdgxU4
-    int sum_flow=0;
-    int* visited=malloc(num_node*sizeof(int));
+    // Phase2: Ford-Fulkerson (simplified)
+    // Reference: https://www.youtube.com/watch?v=GiN3jRdgxU4
+    int sum_flow = 0;
+    int* visited = malloc(num_node * sizeof(int));
     int vertex;
-    queue traversal=NULL;
-    stack cur, full_path=NULL, node_pair=NULL;
-    for(;;){
-        //Reset
-        for(i=0; i<num_node; i++){
-            visited[i]=0;
+    queue traversal = NULL;
+    stack cur, full_path = NULL, node_pair = NULL;
+    for (;;) {
+        // Reset
+        for (i = 0; i < num_node; i++) {
+            visited[i] = 0;
         }
-        full_path=node_pair=NULL;
-        traversal=NULL;
+        full_path = node_pair = NULL;
+        traversal = NULL;
 
-        //Use BFS find edge from 0 to num_node-1
-        enq(&traversal,0);
-        visited[0]=1;
+        // Use BFS find edge from 0 to num_node-1
+        enq(&traversal, 0);
+        visited[0] = 1;
         printf("Find path:\n");
-        while(traversal){
-            vertex=deq(&traversal);
-            for(i=0; i<num_node; i++){
-                if(adj_matrix[vertex][i]>0 && visited[i]==0){
+        while (traversal) {
+            vertex = deq(&traversal);
+            for (i = 0; i < num_node; i++) {
+                if (adj_matrix[vertex][i] > 0 && visited[i] == 0) {
                     enq(&traversal, i);
-                    visited[i]=1;
-                    printf("%d->%d\n",vertex,i);
-                    push(&node_pair,vertex,i);
+                    visited[i] = 1;
+                    printf("%d->%d\n", vertex, i);
+                    push(&node_pair, vertex, i);
                 }
             }
         }
 
-        //Find equivalence class from num_node-1 to 0
-        //In order to find one path from 0 to num_node-1
+        // Find equivalence class from num_node-1 to 0
+        // In order to find one path from 0 to num_node-1
         printf("\nEquivalent:\n");
-        int min_rn=INF;
-        vertex=num_node-1;
-        for(cur=node_pair; cur; cur=cur->next){
-            if(cur->j == vertex){
-                push(&full_path,cur->i,cur->j);
+        int min_rn = INF;
+        vertex = num_node - 1;
+        for (cur = node_pair; cur; cur = cur->next) {
+            if (cur->j == vertex) {
+                push(&full_path, cur->i, cur->j);
                 vertex = cur->i;
-                printf("%d->%d residual_num:%d\n",cur->i,cur->j,adj_matrix[cur->i][cur->j]);
-                if(adj_matrix[cur->i][cur->j] < min_rn){
+                printf("%d->%d residual_num:%d\n", cur->i, cur->j,
+                       adj_matrix[cur->i][cur->j]);
+                if (adj_matrix[cur->i][cur->j] < min_rn) {
                     min_rn = adj_matrix[cur->i][cur->j];
                 }
             }
         }
 
-        //Update residual_num
-        for(cur=full_path; cur; cur=cur->next){
-            adj_matrix[cur->i][cur->j]-=min_rn;
+        // Update residual_num
+        for (cur = full_path; cur; cur = cur->next) {
+            adj_matrix[cur->i][cur->j] -= min_rn;
         }
 
-        if(full_path == NULL){
+        if (full_path == NULL) {
             break;
-        }else{
-            sum_flow+=min_rn;
+        } else {
+            sum_flow += min_rn;
         }
 
-        //Clear stack
-        for(cur=node_pair; cur; cur=pop(&cur));
-        for(cur=full_path; cur; cur=pop(&cur));
+        // Clear stack
+        for (cur = node_pair; cur; cur = pop(&cur))
+            ;
+        for (cur = full_path; cur; cur = pop(&cur))
+            ;
         printf("------------------------\n");
     }
 
-    printf("ans:%d\n",sum_flow);
+    printf("ans:%d\n", sum_flow);
 
-//Phase3: Write file
-    f=fopen("output3.txt","w");
-    fprintf(f,"%d", sum_flow);
+    // Phase3: Write file
+    f = fopen("output3.txt", "w");
+    fprintf(f, "%d", sum_flow);
     fclose(f);
 
     system("pause");
