74cb307d8065381199c7be07da0e30d3c84f3916
liao2000 <liao2000@livemail.tw>
2022-01-30 00:21:16 +0800

    Add Delete()

--- Tree/AVL-Tree.c
+++ Tree/AVL-Tree.c
@@ -1,5 +1,8 @@
 // This is an example for AVL tree
-// 2021/10/24
+// Use stack instead of recursive
+//
+// avl_insert: 2021/10/24
+// avl_delete: 2022/01/30
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,19 +15,56 @@ typedef struct treeNode {
     int height;
 } TreeNode;
 
+typedef struct stack {
+    TreeNode*** list;
+    unsigned int cap;
+    int top;
+} Stack;
+
+Stack* stack_create() {
+    Stack* new = malloc(sizeof(Stack));
+    new->cap = 8;
+    new->top = -1;
+    new->list = malloc(sizeof(TreeNode**) * new->cap);
+}
+
+// the function is used to record the path when traversing
+void stack_push(Stack* this, TreeNode** element) {
+    this->top++;
+    if (this->top == this->cap) {
+        this->cap = this->cap << 1;
+        TreeNode*** newList = malloc(sizeof(TreeNode**) * this->cap);
+        memcpy(newList, this->list, this->top * sizeof(TreeNode**));
+    }
+    this->list[this->top] = element;
+}
+
+TreeNode** stack_pop(Stack* this) {
+    return this->list[this->top--];
+}
+
+// return 0 if stack is not empty
+int stack_empty(Stack* this) {
+    return this->top == -1;
+}
+
+// make stack be empty
+void stack_init(Stack* this) {
+    this->top = -1;
+}
+
 // implement your own comapre function
-// return 1 if el1 is less than el2
-typedef int (*Less_func)(void* el1, void* el2);
+// return negative if el1 is less than el2
+// return 0 if el1 is equal to el2
+// return positive if el1 is greater than el2
+typedef int (*Compare_func)(void* el1, void* el2);
 
 typedef struct avlTree {
     TreeNode* root;
-    Less_func less;
+    Compare_func cmp;
     void (*insert)(struct avlTree* this, void* element);
-    // traverse
-    TreeNode*** stack;
-    unsigned int stack_len;
-    unsigned int stack_logLen;
-    int stack_top;
+    void (*delete)(struct avlTree* this, void* element);
+    Stack* stack;
 } AVLTree;
 
 void treeNode_updateHeight(TreeNode* this) {
@@ -101,80 +141,139 @@ void treeNode_reBalance(TreeNode** this) {
 }
 
 void avlTree_insert(AVLTree* this, void* element) {
+    // create new node
     TreeNode* node = malloc(sizeof(TreeNode));
     node->element = element;
     node->left = NULL;
     node->right = NULL;
     node->height = 1;
 
-    // `this` has root
-    if (this->root) {
-        TreeNode* current = NULL;
-
-        // push root node
-        this->stack[++this->stack_top] = &this->root;
-        for (current = this->root; current;) {
-            // use a stack which is implemented by arrary list for saving path
-            this->stack_top++;
-            if (this->stack_top == this->stack_len) {
-                TreeNode*** newList =
-                    malloc(sizeof(TreeNode**) << (this->stack_logLen + 1));
-                memcpy(newList, this->stack,
-                       this->stack_len * sizeof(TreeNode**));
-                this->stack_len = this->stack_len << 1;
-                this->stack_logLen = this->stack_logLen + 1;
-            }
-
-            // find path and push node to stack
-            if (this->less(element, current->element)) {
-                this->stack[this->stack_top] = &current->left;
-                current = current->left;
-            } else {
-                this->stack[this->stack_top] = &current->right;
-                current = current->right;
-            }
-        }
-
-        // pop stack to updateHeight() and reBlance()
-        // skip the first popped node
-        this->stack_top--;
-
-        TreeNode** visit;
-
-        // insert into node
-        if (this->stack_top != -1) {
-            visit = this->stack[this->stack_top--];
-            if (*visit && this->less(element, (*visit)->element)) {
-                (*visit)->left = node;
-            } else {
-                (*visit)->right = node;
-            }
-            treeNode_updateHeight(*visit);
-            treeNode_reBalance(visit);
-        }
-
-        // other nodes
-        while (this->stack_top != -1) {
-            visit = this->stack[this->stack_top--];
-            treeNode_updateHeight(*visit);
-            treeNode_reBalance(visit);
-        }
-    } else {
-        // `this` does not have root
+    // if this avl tree does not have any nodes.
+    if (!this->root) {
         this->root = node;
+        return;
+    }
+
+    // find the node where we can add new node.
+    // when finding, record the path at the same time
+
+    TreeNode* current = NULL;
+    stack_init(this->stack);
+    stack_push(this->stack, &this->root);
+    for (current = this->root; current;) {
+        // find path and push node to stack
+        if (this->cmp(element, current->element) < 0) {
+            stack_push(this->stack, &current->left);
+            current = current->left;
+        } else {
+            stack_push(this->stack, &current->right);
+            current = current->right;
+        }
+    }
+
+    // `super_current` is the pointer of the left or right
+    // in the parent node of `current`
+    //
+    // for example
+    //          parent
+    //        /        \
+    //      left      right
+    //      /
+    //    node
+    //
+    // current = node
+    // super_current = &left
+    // *super_current = current
+
+    TreeNode** super_current = stack_pop(this->stack);
+
+    // insert into node
+    *super_current = node;
+
+    // rebalance other nodes
+    // super_current is used to just a temporary variable in the while block
+    while (!stack_empty(this->stack)) {
+        super_current = stack_pop(this->stack);
+        treeNode_updateHeight(*super_current);
+        treeNode_reBalance(super_current);
     }
 }
 
+// reference:
+// https://blog.csdn.net/qq_21388535/article/details/105601270
+void avlTree_delete(AVLTree* this, void* element) {
+    // find the node we want do delete
+    // when finding, record the path at the same time
+
+    TreeNode* current = NULL;
+    stack_init(this->stack);
+    stack_push(this->stack, &this->root);
+    for (current = this->root; current;) {
+        if (this->cmp(element, current->element) < 0) {
+            stack_push(this->stack, &current->left);
+            current = current->left;
+        } else if (this->cmp(element, current->element) > 0) {
+            stack_push(this->stack, &current->right);
+            current = current->right;
+        } else {
+            // found!
+            break;
+        }
+    }
+
+    // if not found
+    if (!current) {
+        fprintf(stderr, "delete node is not in the tree");
+        exit(EXIT_FAILURE);
+    }
+
+    // `super_current` is the pointer of the left or right
+    // in the parent node of current
+    // same usage in function avlTree_insert()
+    TreeNode** super_current = stack_pop(this->stack);
+
+    if (!current->right || !current->left) {
+        *super_current = (current->right) ? current->right : current->left;
+        free(current);
+    } else {
+        // find successor: the smallest value in right subtree
+        // when finding successor, record the path
+        stack_push(this->stack, super_current);
+        stack_push(this->stack, &current->right);
+
+        TreeNode* successor;
+        for (successor = current->right; successor->left;) {
+            stack_push(this->stack, &successor->left);
+            successor = successor->left;
+        }
+
+        TreeNode** super_successor = stack_pop(this->stack);
+
+        // replace current node to successor node
+        current->element = successor->element;
+        *super_successor = successor->right;
+
+        free(successor);
+    }
+
+    // rebalance other nodes
+    // super_current is used to just a temporary variable in the while block
+    while (!stack_empty(this->stack)) {
+        super_current = stack_pop(this->stack);
+        treeNode_updateHeight(*super_current);
+        treeNode_reBalance(super_current);
+    }
+    return;
+}
+
 // build an empty binary tree
-AVLTree* __AVLTree__(Less_func less) {
+AVLTree* avlTree_create(Compare_func cmp) {
     AVLTree* new = malloc(sizeof(AVLTree));
     new->root = NULL;
-    new->less = less;
+    new->cmp = cmp;
     new->insert = avlTree_insert;
-    new->stack = malloc(sizeof(TreeNode**) << 3);
-    new->stack_len = 8;
-    new->stack_logLen = 3;
-    new->stack_top = -1;
+    new->delete = avlTree_delete;
+    new->stack = stack_create();
     return new;
 }
 
@@ -185,13 +284,13 @@ struct person {
     int tall;
 };
 
-int personLessFunc(void* el1, void* el2) {
-    return ((struct person*)el1)->tall < ((struct person*)el2)->tall;
+int personCmpFunc(void* el1, void* el2) {
+    return (((struct person*)el1)->tall - ((struct person*)el2)->tall);
 }
 
-void inorder(TreeNode* root) {
+void print_detail(TreeNode* root) {
     if (root) {
-        inorder(root->left);
+        print_detail(root->left);
         struct person* element = (struct person*)(root->element);
         printf("(%s, %d) height: %d\n", element->name, element->tall,
                root->height);
@@ -207,21 +306,31 @@ void inorder(TreeNode* root) {
         } else {
             printf("    right: NULL\n");
         }
-        inorder(root->right);
+        print_detail(root->right);
+    }
+}
+
+void print(TreeNode* root) {
+    if (root) {
+        print(root->left);
+        struct person* element = (struct person*)(root->element);
+        printf("(%s, %d) -> ", element->name, element->tall);
+        print(root->right);
     }
 }
 
 int main() {
     // Prepare data
     struct person list[] = {
-        {"Rina", 149},   {"Hanamaru", 152}, {"Nico", 154},   {"Chisato", 155},
-        {"Hanayo", 156}, {"Honoka", 157},   {"Kanata", 158}, {"Kotori", 159},
-        {"Riko", 160},   {"Maki", 161},     {"Eli", 162},    {"Mari", 163},
-        {"Karin", 167}};
+        {"Hanamaru", 152}, {"Rina", 149},   {"Nico", 154},    {"Hanayo", 156},
+        {"Kanata", 158},   {"Maki", 161},   {"Chisato", 155}, {"Karin", 167},
+        {"Kotori", 159},   {"Honoka", 157}, {"Riko", 160},    {"Eli", 162},
+        {"Mari", 163},
+    };
     int list_len = sizeof(list) / sizeof(list[0]);
 
     // Create binary tree
-    AVLTree* tree = __AVLTree__(personLessFunc);
+    AVLTree* tree = avlTree_create(personCmpFunc);
 
     // Put data into the tree
     int i;
@@ -230,11 +339,15 @@ int main() {
     }
 
     // see the tree
-    inorder(tree->root);
+    print_detail(tree->root);
+    print(tree->root);
 
-    // check
-    printf("stack_len: %d\n", tree->stack_len);
-    printf("stack_logLen: %d", tree->stack_logLen);
-
+    // Delete
+    for (i = 0; i < list_len; i++) {
+        printf("delete %s\n", list[i].name);
+        tree->delete (tree, list + i);
+        print(tree->root);
+        printf("Null\n");
+    }
     return 0;
 }
